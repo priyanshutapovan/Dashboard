793a1e4e6f548dce355a3b6b59710eac0433a7bf
priyanshutapovan <priyanshu.tapovan@gmail.com>
2026-04-22 13:27:12 +0530

    updating in app.js

--- tapovan-morning-screen/src/App.jsx
+++ tapovan-morning-screen/src/App.jsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
 import {
   CashPage,
   ReceivablesPage,
   OrdersPage,
-  DispatchesPage
+  DispatchesPage,
+  CarrierTrackerPage
 } from './pages/DetailPages'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/cash" element={<CashPage />} />
-        <Route path="/receivables" element={<ReceivablesPage />} />
-        <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/dispatches" element={<DispatchesPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/carrier-tracker" element={<CarrierTrackerPage />} />
+          <Route path="/cash" element={<CashPage />} />
+          <Route path="/receivables" element={<ReceivablesPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/dispatches" element={<DispatchesPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
